9f59be2a21c0ac6dbb18ada99688715aa5d5a722
balintsly <sziszko0129@gmail.com>
2022-03-05 17:41:28 +0100

    addtobattle logic layer done

---
+++ SuperheroManager/Logic/SuperheroLogic.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Toolkit.Mvvm.Messaging;
+using SuperheroManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperheroManager.Logic
+{
+    public partial class SuperheroLogic
+    {
+        IList<Superhero> SuperheroesInHQ;
+        IList<Superhero> SuperheroesInBattle;
+        IMessenger Messenger;
+
+        public SuperheroLogic(IList<Superhero> superheroesInHQ, IList<Superhero> superheroesInBattle, IMessenger messenger)
+        {
+            SuperheroesInHQ = superheroesInHQ;
+            SuperheroesInBattle = superheroesInBattle;
+            Messenger = messenger;
+        }
+        public void AddToBattle(Superhero superhero)
+        {
+            SuperheroesInBattle.Add(superhero);
+            Messenger.Send("SuperheroAdded", "SuperheroInfo");
+        }
+    }
+}
