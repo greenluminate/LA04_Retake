419ecb5ffbc6cd295c6751d6bdccc32dbef767e6
balintsly <sziszko0129@gmail.com>
2022-03-05 19:19:37 +0100

    number to color converter

--- SuperheroManager/Helpers/NumberToColorConverter.cs
+++ SuperheroManager/Helpers/NumberToColorConverter.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
 
 namespace SuperheroManager.Helpers
 {
-    public partial class NumberToColorConverter
+    public partial class NumberToColorConverter : IValueConverter
     {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double asd=double.Parse(value.ToString());
+            if (asd<4)
+            {
+                return Brushes.Red;
+            }
+            else if (asd < 8)
+            {
+                return Brushes.Yellow;
+            }
+            else
+            {
+                return Brushes.Green;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
