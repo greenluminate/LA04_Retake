7238035622372128db3ffb8a9be77a857e074565
Q4NSIQ <Q4NSIQ@stud.uni-obuda.hu>
2022-03-05 21:33:07 +0100

    Main Window cs cleared

--- SuperheroManager/MainWindow.xaml.cs
+++ SuperheroManager/MainWindow.xaml.cs
@@ -23,8 +23,6 @@ namespace SuperheroManager
         public MainWindow()
         {
             InitializeComponent();
-            CreateSuperheroWindow csw = new CreateSuperheroWindow();
-            csw.ShowDialog();
         }
     }
 }
