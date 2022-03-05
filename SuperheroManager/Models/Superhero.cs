e123af5e5a7d40c9141b8364b51f23d3d622976d
Q4NSIQ <Q4NSIQ@stud.uni-obuda.hu>
2022-03-05 16:54:23 +0100

    Superhero model created

---
+++ SuperheroManager/Models/Superhero.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperheroManager.Models
+{
+    public partial class Superhero : ObservableObject
+    {
+        private SideEnum side;
+
+        public SideEnum Side
+        {
+            get { return side; }
+            set { SetProperty( ref side, value); }
+        }
+
+        private int speed;
+
+        public int Speed
+        {
+            get { return speed; }
+            set { SetProperty(ref speed, value); }
+        }
+
+        private int power;
+
+        public int Power
+        {
+            get { return power; }
+            set { SetProperty(ref power, value); }
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { SetProperty(ref name, value); }
+        }
+    }
+}
